d29ac2857b0a0e61f66143640b6cf052196dc6bc
Almir Fernandes <80928100+almirfpereira@users.noreply.github.com>
2022-06-17 18:24:32 -0300

    first-commit

---
+++ js/main.js
@@ -0,0 +1,127 @@
+ /*
+	Indus by TEMPLATE STOCK
+	templatestock.co @templatestock
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+/* ------------------------------------------------------------------------------
+ This is jquery module for main page
+ ------------------------------------------------------------------------------ */
+
+ /* Global constants */
+
+ /*global jQuery */
+ jQuery(function ($) {
+  'use strict';
+
+
+
+  var App = {
+    $options: {},
+    $loader: $(".loader"),
+    $animationload: $(".animationload"),
+    $countdown: $('#countdown_dashboard'),
+
+    bindEvents: function() {
+      //binding events
+      $(window).on('load', this.load.bind(this));
+      $(document).on('ready', this.docReady.bind(this));
+    },
+    load: function() {
+      /* ==============================================
+      1.Page Preloader
+      =============================================== */
+      this.$loader.delay(300).fadeOut();
+      this.$animationload.delay(600).fadeOut("slow");
+    },
+    docReady: function() {
+      /* -----------------------------------------------------------------------
+        Countdown
+        ----------------------------------------------------------------------- */
+        this.$countdown.countDown({
+          targetDate: {
+            'day':    this.$options.day,
+            'month':  this.$options.month,
+            'year':   this.$options.year,
+            'hour':   this.$options.hour,
+            'min':    this.$options.min,
+            'sec':    this.$options.sec
+          },
+          omitWeeks: true
+        });
+
+      /* ==============================================
+      NiceScroll
+      =============================================== */
+      $("html").niceScroll({
+        scrollspeed: 50,
+        mousescrollstep: 38,
+        cursorwidth: 7,
+        cursorborder: 0,
+        autohidemode: true,
+        zindex: 9999999,
+        horizrailenabled: false,
+        cursorborderradius: 0
+      });
+
+      /* ==============================================
+      Parallax
+      =============================================== */
+      $(window).stellar({
+        horizontalScrolling: false,
+        responsive: true,
+        scrollProperty: 'scroll',
+        parallaxElements: false,
+        horizontalOffset: 0,
+        verticalOffset: 0
+      });
+
+      //custom app
+      
+    },
+    init: function (_options) {
+      $.extend(this.$options, _options);
+      this.bindEvents();
+    }
+  }
+
+  //Initializing the app
+  //passing the countdown date
+  App.init({day: 25, month: 6, year: 2022, hour: 19, min: 59, sec: 59});
+});
+
+// Carrousel
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
+});
+
